db339ef77de45ad99617df3f4ac73f0edee72966
22ISendP-2 - Антон <Urlilka@yandex.ru>
2025-05-29 13:33:56 +0500

    Добавленны cert_panel.html, recom_panel.html Доработанны app.py, base.html, stud_panel.html Изменён Фон в base.html Добавлена таблица в stud_panel.html Добавленна таблица в cert_panel.html Добавлен аккордион в recom_panel.html В app.py добавлены маршруты в новые части В stud_panel.html добавлена кнопка "Выход" с прикреплённым соответствующим действием В recom_panel.html и cert_panel.html добавлены кнопки "Назад" ведущие в stud_panel.html

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
 from crypt import methods
-
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
-
 from Controllers.User_Controllers import User_Controller
 from Models.Users import Users
+
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.secret_key = "00000"
 
+# ---------------------------------------------------------------------
+# Активный пользователь
 @login_manager.user_loader
 def load_user(user_id):
     return User_Controller.show(int(user_id))
+# ---------------------------------------------------------------------
 
+# ---------------------------------------------------------------------
 # Метод выхода
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect("/")
+# ---------------------------------------------------------------------
 
+# ---------------------------------------------------------------------
 # Перенаправление неавторизованных в корень
 @login_manager.unauthorized_handler
 def unauthorized():
     if not current_user.is_authenticated:
         return redirect("/")
+# ---------------------------------------------------------------------
 
+# ---------------------------------------------------------------------
 # Корень сайта / Главная страница
 @app.route("/", methods = ['POST','GET'])
 def login():
@@ -51,18 +58,61 @@ def login():
         else:
             print("Nope nope!!!")
     return render_template("index.html", title=title)
+# ---------------------------------------------------------------------
 
+# ---------------------------------------------------------------------
+# Панель студента
 @app.route("/student_panel")
 @login_required
 def student():
-    title = f"Панель студента. Группа {Users.role_id.role}"
     if current_user.role_id.role == "Student":
-        pass
-    return render_template(
-        "stud_panel.html",
-        title = title
-    )
+        title = f"Панель студента. Группа {Users.role_id.role}"
 
+        return render_template(
+            "stud_panel.html",
+            title = title
+        )
+    else:
+        return redirect("/logout")
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Панель сертификатов
+# @app.route("/student_panel/certifications/<int:id>")
+@app.route("/student_panel/certifications")
+@login_required
+def certificate():
+    if current_user.role_id.role == "Student":
+        title = f"Cертификаты студента. Группа {Users.role_id.role}"
+
+        return render_template(
+            "cert_panel.html",
+            title = title
+        )
+    else:
+        return redirect("/logout")
+# ---------------------------------------------------------------------
+
+# Панель Рекомендаций
+# @app.route("/student_panel/recomendations/<int:id>")
+@app.route("/student_panel/recomendations")
+@login_required
+def recomendate():
+    if current_user.role_id.role == "Student":
+        title = f"Рекомендации студента. Группа {Users.role_id.role}"
+
+        return render_template(
+            "recom_panel.html",
+            title = title
+        )
+    else:
+        return redirect("/logout")
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Запуск переменной app и веб-сервера
 if __name__ == "__main__":
-    # Запуск переменной app и веб-сервер
+
     app.run(debug=True)
+
+# ---------------------------------------------------------------------
